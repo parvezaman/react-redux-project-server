dfef8e3cbdc0257fa59cc837dd6950fefa477331
parvezaman <emailtoamanullah@gmail.com>
2021-12-19 20:31:07 +0600

    server started

---
+++ index.js
@@ -0,0 +1,37 @@
+const express = require('express');
+const { MongoClient } = require('mongodb');
+const app = express();
+const cors = require('cors');
+require('dotenv').config();
+
+// middleware setup
+app.use(cors());
+app.use(express.json());
+
+
+const port = process.env.PORT || 5000;
+
+//MoingoDB Connection
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.kbuol.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+
+async function run() {
+    try {
+      await client.connect();
+      const test = database.collection("test");
+      
+  
+    } finally {
+      // await client.close();
+    }
+  }
+  run().catch(console.dir);
+  
+  
+  app.get('/', (req, res) => {
+    res.send('Hello World!');
+  })
+  
+  app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`);
+  })
